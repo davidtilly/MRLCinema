062f479d214d00dde4d2d9677f36c0249d43951b
David Tilly <david.tilly@igp.uu.se>
2025-02-07 17:39:10 +0100

    mask handling

---
+++ registration/create_mask.py
@@ -0,0 +1,86 @@
+import numpy as np
+from scipy.ndimage import distance_transform_edt
+import SimpleITK as sitk
+from ..readcine.convert_to_sitk import SliceDirection
+
+##########################################################################
+def distance_map(mask:np.array, spacing:np.array) -> np.array:
+    """ Create distance map such a narrow band can be constructed. """
+    
+    dt_inside = distance_transform_edt(mask, sampling = spacing)
+    dt_outside = distance_transform_edt(1 - mask, sampling = spacing)
+    dt = dt_inside - dt_outside
+    
+    return dt
+
+
+def mask_dilation(mask:np.array, pos_000:np.array, spacing:np.array, dilation_distance) -> np.array:
+    """
+    Create a SimpleITK image from a numpy array. The image is created with the
+    same origin and spacing as the original image.
+
+    :param mask: Numpy array of the mask
+    :param pos_000: Position of the pixel with lowest x, y, z, position
+    :param spacing: Pixel spacing
+    :param dilation_distance: Distance in wolrd units (mm) to dilate the mask
+    :return: the dilated mask
+    """
+
+    dt = distance_map(mask, spacing)
+    dt = dt + dilation_distance 
+    mask_dilated = (dt >= 0).astype(np.uint8)
+
+    return mask_dilated
+
+
+def create_mask_2d(mask:np.array, pos_000:np.array, spacing:np.array, slice_diretion:SliceDirection) -> sitk.Image:
+    """
+    Create a 2D SimpleITK image from a numpy array representation of the mask. 
+    The 2D slice is through the center of the 3D volume in the specified slice direction. 
+
+    :param mask: Numpy array of the mask
+    :param pos_000: Position of the pixel with lowest x, y, z, position
+    :param spacing: Pixel spacing
+    :return: The dilated mask
+    """
+    
+    dim = mask.shape
+
+    # cut image depending on the slice direction
+    if slice_diretion == SliceDirection.TRANSVERSAL:
+        
+        slice = mask[:, :, int(dim[2]/2)]
+        pos_00 =[pos_000[0], pos_000[1]]
+        spacing_2d = [spacing[0], spacing[1]]
+
+    elif slice_diretion == SliceDirection.SAGITTAL:
+        
+        slice = mask[:, int(dim[1]/2), :]
+        pos_00 =[pos_000[0], pos_000[2]]
+        spacing_2d = [spacing[0], spacing[2]]
+
+    elif slice_diretion == SliceDirection.CORONAL:
+        
+        slice = mask[int(dim[0]/2), :, :]
+        pos_00 =[pos_000[1], pos_000[2]]
+        spacing_2d = [spacing[1], spacing[2]]
+
+    else:
+        raise ValueError(f'Unknown slice direction {slice_diretion}')
+    
+    sitk_image = sitk.GetImageFromArray(np.swapaxes(slice, 0, 1))
+
+    # assign the geometry
+    sitk_image.SetOrigin(pos_00)
+    sitk_image.SetSpacing(spacing_2d)
+
+    return sitk_image
+
+
+def remove_center_cross(mask:np.array, pos_00:np.array, spacing:np.array, block_size:int) -> np.array:
+
+    dim = mask.shape
+    center = [int(dim[0]/2), int(dim[1]/2)]
+    mask_remove = mask.copy()
+    mask_remove[center[0]-block_size:center[0]+block_size, center[1]-block_size:center[1]+block_size] = 0
+    return mask_remove
