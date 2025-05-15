24222f28399f58f289e825ab504969e29cd3ebff
David Tilly <david.tilly@igp.uu.se>
2025-05-15 10:37:43 +0200

    WIP set up run of patients

---
+++ patient_data.py
@@ -0,0 +1,68 @@
+from U2Dose.dicomio.rtstruct import RtStruct
+from U2Dose.dicomio.rtplan import RtPlan
+import pydicom
+import glob, os
+import json
+
+
+#############################################################################
+def read_cine_patient_ID(path) -> str:
+    """ Read the patient ID from the current directory
+
+    :return: The patient ID
+    """
+    mask_filenames = glob.glob(os.path.join(path, 'BinaryMasks', 'Z_MM*.json'))
+
+    if len(mask_filenames) == 0:
+        dir = os.path.join(path, 'BinaryMasks')
+        raise FileNotFoundError(f'No mask files found in the dir {dir}')
+
+    mask_filename = mask_filenames[0]
+    with open(mask_filename) as f:
+        mask_file = json.load(f)
+        return mask_file['PatientMRN']
+
+#############################################################################
+def find_cine_frame_of_reference(path) -> str:
+    """ Read the frame of reference from the session
+    """
+    mask_filenames = glob.glob(os.path.join(path, 'BinaryMasks', 'Z_MM*.json'))
+
+    if len(mask_filenames) == 0:
+        dir = os.path.join(path, 'BinaryMasks')
+        raise FileNotFoundError(f'No mask files found in the dir {dir}')
+
+    mask_filename = mask_filenames[0]
+    with open(mask_filename) as f:
+        mask_file = json.load(f)
+        return mask_file['FrameOfReferenceUid']
+
+############################################################################
+def find_structure_set(patient_data_root:str, patient_ID:str, frame_of_reference:str) -> RtStruct|None:
+    """ Find the RT structure set for a given patient ID and frame of reference."""
+
+    rtss_filenames = glob.glob(os.path.join(patient_data_root, patient_ID, '*', 'RS*.dcm'))
+    for filename in rtss_filenames:
+        rtss_ds = pydicom.dcmread(filename)
+        
+        if rtss_ds.ReferencedFrameOfReferenceSequence[0].FrameOfReferenceUID == frame_of_reference:
+            rtss = RtStruct(filename)
+            rtss.parse()
+            return rtss
+        
+    return None
+
+############################################################################
+def find_plan(patient_data_root:str, patient_ID:str, frame_of_reference:str) -> RtPlan|None:
+    """ Find the RT plan for a given patient ID and fraction."""
+
+    rtplan_filenames = glob.glob(os.path.join(patient_data_root, patient_ID, '*', 'RP*.dcm'))
+    for filename in rtplan_filenames:
+        rtplan_ds = pydicom.dcmread(filename)
+        
+        if rtplan_ds.FrameOfReferenceUID == frame_of_reference:
+            rtplan = RtPlan(filename)
+            rtplan.parse()
+            return rtplan
+        
+    return None
--- readcine/read_mask.py
+++ readcine/read_mask.py
@@ -2,19 +2,9 @@
 import glob, os
 import json
 
-# read patient ID
-def read_patient_ID(path):
-    """ Read the patient ID from the current directory
-
-    :return: The patient ID
-    """
-    mask_filename = glob.glob(os.path.join(path, 'BinaryMasks', 'Z_MM*.json'))
-    mask_filename = mask_filename[0]
-    with open(mask_filename) as f:
-        mask_file = json.load(f)
-        return mask_file['PatientMRN']
     
 
+#############################################################################
 def read_mask(filename):
     """ Read the mask file from the current directory
 
@@ -22,7 +12,6 @@ def read_mask(filename):
     """
     with open(filename) as f:
         mask_json = json.load(f)
-
 
         mask_meta_data = mask_json['StructureMask']
         dim =[mask_meta_data['VoxelsAlongX'], mask_meta_data['VoxelsAlongY'], mask_meta_data['VoxelsAlongZ']]
