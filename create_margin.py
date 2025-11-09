1796ab71a0e058a80f07ff956ad37e201c78d732
David Tilly <david.tilly@igp.uu.se>
2025-11-09 19:50:23 +0100

    Updated doc

--- create_margin.py
+++ create_margin.py
@@ -4,14 +4,21 @@ import numpy as np
 def create_margin(mask:sitk.Image, margin:np.array) -> sitk.Image:
     """ Create a new binary image using a margin 
 
+    Note! The binary mask is expanded with an integer number of voxels.
+    if specified margin is not an even number of voxels, then the number of 
+    voxels is rounded to nearest integer. 
+
     Note! Mask must be boolean with foreground = 1 and background = 0.
     If not then the function has undefined behaviour but will not throw.
 
     :param mask: mask to be expanded
-    :param margin: margin to expand (mm)
+    :param margin: margin to expand [x, y, z] (mm)
     :return expanded volume
     """ 
 
+    if len(margin) != 3:
+        raise ValueError(f'Margin must be specified with a single number in x, y, z (vector of length 3) {margin}')
+    
     pixel_radius = margin / np.array(mask.GetSpacing())
     pixel_radius = (pixel_radius + 0.5).astype(int)
 
